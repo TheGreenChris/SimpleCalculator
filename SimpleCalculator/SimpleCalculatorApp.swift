11bdc495fdb653af776e4e45979a0be73d9d5826
Christopher Richard <thegreenchris@icloud.com>
2023-09-30 21:01:55 +0200

    Initial Commit

---
+++ SimpleCalculator/SimpleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculatorApp.swift
+//  SimpleCalculator
+//
+//  Created by Christopher Richard on 30.09.23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
